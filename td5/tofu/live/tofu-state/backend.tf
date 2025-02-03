5d03155cc8ba1a6c122aa89ab6bd17e4ec47d7c4
LiYichenESIEE <151749813+LiYichenESIEE@users.noreply.github.com>
2025-02-03 21:29:44 +0800

    "test"

--- td5/tofu/live/tofu-state/backend.tf
+++ td5/tofu/live/tofu-state/backend.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     # TODO: fill in your own bucket name here!
     bucket         = "fundamentals-of-devops-tofu-state" 
-    key            = "ch5/tofu/live/tofu-state"          
-    region         = "us-east-2"                         
-    encrypt        = true                                
+    key            = "td5/tofu/live/lambda-sample"       
+    region         = "us-east-2"
+    encrypt        = true
     # TODO: fill in your own DynamoDB table name here!
     dynamodb_table = "fundamentals-of-devops-tofu-state" 
   }
